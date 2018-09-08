7354700190b4a8a25831b5a80a398caf87a7b22f
Eiad Al-Khanshour <eiadxp@hotmail.com>
2018-09-08 10:19:05 +0300

    Minor Fixes

--- ClinicalOffice.ValidationFramework/ValidationError.cs
+++ ClinicalOffice.ValidationFramework/ValidationError.cs
@@ -5,9 +5,10 @@ namespace ClinicalOffice.ValidationFramework
     public class ValidationError
     {
         internal static ValidationError[] EmptyArray = Array.Empty<ValidationError>();
+        private string _errorMessage;
 
         public string PropertyName { get; private set; }
-        public string ErrorMessage { get; internal set; }
+        public string ErrorMessage { get => _errorMessage; internal set => _errorMessage = value?.Trim(); }
         public bool HasError { get => !string.IsNullOrWhiteSpace(ErrorMessage); }
 
         public ValidationError(string propertyName, string errorMessage = null)
